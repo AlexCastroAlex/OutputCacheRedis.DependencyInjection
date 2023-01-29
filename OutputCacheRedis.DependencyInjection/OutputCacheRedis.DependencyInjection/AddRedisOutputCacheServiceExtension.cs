344da157d113fcf911c86a9925b47d5e12a2f439
alexandre.castro <alexandre.castro@exakis-nelite.com>
2023-01-29 06:53:05 +0100

    initial commit

---
+++ OutputCacheRedis.DependencyInjection/OutputCacheRedis.DependencyInjection/AddRedisOutputCacheServiceExtension.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.OutputCaching;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OutputCacheRedis.DependencyInjection
+{
+    public static class AddRedisOutputCacheServiceExtension
+    {
+        public static IServiceCollection AddRedisOutputCache(this IServiceCollection services, string RedisConnectionString)
+        {
+            ArgumentNullException.ThrowIfNull(services);
+            ArgumentNullException.ThrowIfNull(RedisConnectionString);
+
+            services.AddSingleton<IConnectionMultiplexer>(_ => ConnectionMultiplexer.Connect(RedisConnectionString));
+            // Add required services        
+            services.AddOutputCache();
+
+            // Remove default IOutputCacheStore
+            services.RemoveAll<IOutputCacheStore>();
+
+            // Add custom IOutputCacheStore
+            services.AddSingleton<IOutputCacheStore, RedisOutputCacheStore>();
+
+            return services;
+        }
+    }
+}
